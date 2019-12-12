6cbdde2819e0e897c54aac223a3694060a7a7169
toirai <larsalex8@gmail.com>
2019-12-12 20:45:11 +0900

    [finish]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,40 @@
 class BooksController < ApplicationController
   def top
   end
-
   def index
+  	@books=Book.all
+  	@book=Book.new
+   end
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+  	  redirect_to book_path(book.id),notice:"Book was successfully created."
+    else
+      @book = book
+      @books = Book.all
+  	  render :index
+    end
   end
-
   def show
+  	@book=Book.find(params[:id])
+
   end
 
   def edit
+  	@book=Book.find(params[:id])
+  end
+  def update
+  	book=Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id),notice:"Book was successfully update."
+  end
+  def destroy
+  	book=Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path,notice:"Book was successfully delete."
+  end
+  private
+  def book_params
+  	params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+root to: "books#top"
+resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
